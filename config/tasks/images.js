7697bddfc3648f1dc18dec85d877808e6209e82f
Edvard <pmkstudio77@gmail.com>
2025-07-02 01:03:37 +0300

    Исправил пути к images

--- config/tasks/images.js
+++ config/tasks/images.js
@@ -3,11 +3,11 @@ import webp from "gulp-webp";
 // Оптимизация изображений (WebP)
 export const images = () =>
     app.gulp.src(app.paths.src.images, { encoding: false })
-        .pipe(app.plugins.newer("dist/assets/img"))
-        .pipe(app.gulp.dest("dist/assets/img"))
-        .pipe(app.plugins.newer("dist/assets/img"))
+        .pipe(app.plugins.newer(app.paths.build.images))
+        .pipe(app.gulp.dest(app.paths.build.images))
+        .pipe(app.plugins.newer(app.paths.build.images))
         .pipe(app.plugins.rename((path) => {
             path.extname += path.extname;
         }))
         .pipe(webp())
-        .pipe(app.gulp.dest("dist/assets/img"));
+        .pipe(app.gulp.dest(app.paths.build.images));
